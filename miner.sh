3a737be8c0b290788860407209905d9a1ec70725
BadGuy <badguy5525@gmail.com>
2017-05-31 19:39:40 -0300

    Modification for only Minergate XMR mining

---
+++ miner.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "BadGuy Mining Script!"
+echo "By: https://github.com/BadGuy552"
+if [ ! "$1" ]
+then
+read -p "Your Wallet: " wallet
+else
+wallet=$1
+fi
+
+echo "Installing dependences.."
+sudo apt-get install screen build-essential automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ -y 1>/dev/null 2>/dev/null || install=false
+if [ "$install" = false ]
+then
+echo "Error to install dependences, Aborting..."
+exit 1
+fi
+export MINERPATH=$HOME/miner
+mkdir $MINERPATH 1>/dev/null 2>/dev/null
+# CPUMINER: https://github.com/tpruvot/cpuminer-multi
+echo "Downloading Miner... (tpruvot CPUMINER)"
+git clone https://github.com/tpruvot/cpuminer-multi.git $MINERPATH > /dev/null
+
+echo "Compiling Miner.... (maybe this take a long time)"
+
+cd $MINERPATH && ./autogen.sh
+cd $MINERPATH && ./configure --with-crypto --with-curl CFLAGS="-march=native" || configure=false
+cd $MINERPATH && make || compile=false
+if [ "$configure" = false ]
+then
+echo "Error to configure source to build, Aborting..."
+exit 1
+fi
+
+if [ "$compile" = false ]
+then
+echo "Error to compile source, Aborting..."
+exit 1
+fi
+clear
+
+cd $MINERPATH && screen ./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u $wallet -p x --cpu-priority 5
